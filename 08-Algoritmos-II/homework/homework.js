1cd7a14003e6c365bcc54773ab333ab900db42d7
LeanKrn <leandrotomask@gmail.com>
2022-11-21 12:23:57 -0300

    08 update

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,23 +6,17 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-  if(array.length < 1){
-    return [];
-  }
-  else{
-    let left = []; 
-    let right = [];
-    let pivot = array[0]
-    for (let i = 1; i < array.length; i++) {
-      if(array[i] > pivot){
-        right.push(array[i])
-      }
-      else{left.push(array[i])}
-    }
-    return [].concat(quickSort(left), pivot,quickSort(right))
-  }
+  if(array.length <= 1)return array;
 
-
+   let left = [];     //creo un array nuevo 
+   let right = [];    //creo un array nuevo 
+   let pivot = array[0] //creo un pivot para guiar mis arrays
+   for (let i = 1; i < array.length; i++) {   //recorro el array que me llega
+     if(array[i] > pivot){right.push(array[i])}   //si hay un numero en el array mas grande q pivot lo manda a right
+     else{left.push(array[i])}    //si no es mayor lo manda a left
+   }
+return [].concat(quickSort(left), pivot,quickSort(right))
+//este return concatena los array pero antes le pide que se llame a la funcion hasta ordenar todo y cuando termine de ordenar los ordena todos
 }
 console.log(quickSort([5, 1, 4, 2, 8,6,9,12]))
 
@@ -40,7 +34,6 @@ function mergeSort(array) {
   var longitud = Math.floor(array.length / 2) //obtiene e lelemento central de la matriz redondeado hacia abajo
   var derecha = array.slice(0, longitud) // elementos en el lado izquierdo
   var izquierda = array.slice(longitud)  // elementos en el lado derecho
-
   return merge(mergeSort(izquierda), mergeSort(derecha));
 }
 
